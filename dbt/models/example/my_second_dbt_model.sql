0f8286c07e6eaef8e8e0e652fbf960d7ca052848
matthh9797 <matthewh@netextremity.com>
2023-07-04 10:24:01 +0100

    test cd

--- dbt/models/example/my_second_dbt_model.sql
+++ dbt/models/example/my_second_dbt_model.sql
@@ -2,6 +2,6 @@
 -- Use the `ref` function to select from other models
 {{ config(materialized='view') }}
 
-select *
+select *, true as test_passed
 from {{ ref('my_first_dbt_model') }}
 where id = 1
